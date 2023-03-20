72893e49c724a79ab8815732abd049758bc82e2f
Dilum Induwara Rathnayaka <110888028+LearnCsWithDIR@users.noreply.github.com>
2023-03-20 23:09:22 +0530

    Update yolo_basic.py

--- yolo_basic.py
+++ yolo_basic.py
@@ -5,9 +5,9 @@ import cv2
 
 # only one type Yolo file can be run in one time
 
-model1 = YOLO('yolov8n.pt')  # n is nano (detection minimum details)| 8 is version
-#model2 = YOLO('yolov8m.pt')  # m is medium (detection medium details) | 8 is version
-#model3 = YOLO('yolov8l.pt')  # l is large (detection large details) | 8 is version
+model1 = YOLO('yolov8n.pt')  # n is nano (detection minimum details)| v8 is version 8
+#model2 = YOLO('yolov8m.pt')  # m is medium (detection medium details) | v8 is version 8
+#model3 = YOLO('yolov8l.pt')  # l is large (detection large details) | v8 is version 8
 
 results1 = model1("Images/animal_img.jpg",show = True)
 #results2 = model2("Images/person_img.jpg",show = True)
